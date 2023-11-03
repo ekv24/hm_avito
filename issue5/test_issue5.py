b3056e796b626fff4ea3843a8829a726fe37a5e6
ekv24 <ekv24@mail.ru>
2023-11-03 22:56:04 +0300

    some changes

--- issue5/test_issue5.py
+++ issue5/test_issue5.py
@@ -10,21 +10,21 @@ def mock_open():
 
 
 def test_common1(mock_open):
-    cur_date = b'{"currentDateTime": "2023-03-01"}'
+    cur_date = '{"currentDateTime": "2023-03-01"}'
     mock_open.return_value.__enter__.return_value.read.return_value = cur_date
     year = what_is_year_now()
     assert year == 2023
 
 
 def test_common2(mock_open):
-    cur_date = b'{"currentDateTime": "01.03.2019"}'
+    cur_date = '{"currentDateTime": "01.03.2019"}'
     mock_open.return_value.__enter__.return_value.read.return_value = cur_date
     year = what_is_year_now()
     assert year == 2019
 
 
 def test_exception(mock_open):
-    cur_date = b'{"currentDateTime": "01@03@2019"}'
+    cur_date = '{"currentDateTime": "01@03@2019"}'
     mock_open.return_value.__enter__.return_value.read.return_value = cur_date
     with pytest.raises(ValueError):
         what_is_year_now()
